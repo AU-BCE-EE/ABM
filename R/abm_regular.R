616f5c1c55059e5dbf7c20b5fdd68d534821cc68
Sasha D. Hafner <sasha.hafner@gmail.com>
2020-10-30 13:23:29 -0400

    Update default pars

--- R/abm_regular.R
+++ R/abm_regular.R
@@ -3,7 +3,7 @@ function(days, delta_t, pars, starting = NULL, temp_C_fun = temp_C_fun, pH_fun =
 
   # Cannot have no slurry present because is used in all concentration calculations (NTS: could change this)
   if (pars$slurry_mass == 0) {
-    pars$slurry_mass <- 1E-6
+    pars$slurry_mass <- 1E-10
   }
 
   # Sort out emptying interval and related vars
@@ -77,7 +77,7 @@ function(days, delta_t, pars, starting = NULL, temp_C_fun = temp_C_fun, pH_fun =
     resid_xa <- logistic(logit(resid_frac) + pars$resid_enrich)
 
     y[1:n_mic] <- resid_xa * y[1:n_mic]
-    y['slurry_mass'] <- resid_frac * y['slurry_mass']
+    y['slurry_mass'] <- max(resid_frac * y['slurry_mass'], 1E-10)
     y['Sp'] <- resid_frac * y['Sp']
     y['VFA'] <- resid_frac * y['VFA']
     y['sulfate'] <- resid_frac * y['sulfate']

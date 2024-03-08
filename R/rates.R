bf16a25bbd55148000e166cd9c192da95c7d1eb8
Frederik Rask Dalby <au277187@uni.au.dk>
2024-03-08 09:07:59 +0100

    bug

--- R/rates.R
+++ R/rates.R
@@ -118,12 +118,12 @@ rates <- function(t, y, parms, temp_C_fun = temp_C_fun, pH_fun = pH_fun,
     alpha <-  Arrh_func(A, E, R, temp_K)
     
     if(VSd <= 10E-9){
-      scale <- scale.alpha_opt['notVSd']
+      scale['alpha_opt'] <- scale.alpha_opt['notVSd']
     } else if(VSd > 10E-9){
-      scale <- scale.alpha_opt['VSd']
+      scale['alpha_opt']  <- scale.alpha_opt['VSd']
     }
     
-    alpha[names(alpha) != 'urea'] <- scale * alpha[names(alpha) != 'urea']
+    alpha[names(alpha) != 'urea'] <- scale['alpha_opt'] * alpha[names(alpha) != 'urea']
     
     # Microbial substrate utilization rate (vectorized calculation)
     qhat <- scale['qhat_opt'] * CTM_cpp(temp_K, T_opt, T_min, T_max, qhat_opt)

78740ecf1b1ff15d31c4ef8eaeb8cc731740b955
Frederik Rask Dalby <au277187@uni.au.dk>
2024-05-07 12:06:25 +0200

    apply limit for surface respiration, to avoid slow solver

--- R/rates.R
+++ R/rates.R
@@ -204,8 +204,9 @@ rates <- function(t, y, parms, temp_C_fun = temp_C_fun, pH_fun = pH_fun,
     
     if(resp){
      kl.oxygen <- exp(0.6158816 + 0.09205127 * temp_C) # from own lab experiments (Dalby et al. 2023..unpublished) 
-     if (sub_resp <= 0) sub_resp <- 1E-20
-     respiration <- kl.oxygen * area * ((kH_oxygen * 0.208) - 0) * (sub_resp / slurry_mass) / 100
+     # Solver is slowed significantly when the slurry_mass is low relative to surface area is low, therefore cut off at 1 mm slurry height
+     condition <- slurry_mass/area <= 1
+     respiration <- ifelse(condition, 0, respiration <- kl.oxygen * area * ((kH_oxygen * 0.208) - 0) * (sub_resp / slurry_mass) / 100)
     }
     
     # VFA uptake rates

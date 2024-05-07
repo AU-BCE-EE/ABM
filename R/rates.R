7c385d70780d002b6ee7cd0ea56dcfefe91eb944
Frederik Rask Dalby <au277187@uni.au.dk>
2024-05-07 19:51:31 +0200

    fix mistake in less than

--- R/rates.R
+++ R/rates.R
@@ -203,7 +203,7 @@ rates <- function(t, y, parms, temp_C_fun = temp_C_fun, pH_fun = pH_fun,
     sub_resp <- Cfat + CP + RFd + starch + VSd 
     
     # Solver is slowed significantly when the slurry_mass is low relative to surface area is low, therefore cut off at 1 mm slurry height
-    if(resp & slurry_mass/area <= 1){
+    if(resp & slurry_mass/area >= 1){
      kl.oxygen <- exp(0.6158816 + 0.09205127 * temp_C) # from own lab experiments (Dalby et al. 2024..unpublished) 
      respiration <- kl.oxygen * area * ((kH_oxygen * 0.208) - 0) * (sub_resp / slurry_mass) / 100
     }

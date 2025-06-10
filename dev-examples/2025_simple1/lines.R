fa4b8475df1e565f4870003364e4cd81ab48714c
Sasha D. Hafner <sasha@hafnerconsulting.com>
2025-06-10 13:01:46 -0400

    work for HYPUD meeting

--- dev-examples/2025_simple1/lines.R
+++ dev-examples/2025_simple1/lines.R
@@ -8,5 +8,11 @@ dat <- fread('lines.csv')
 dat[, clines := cumsum(lines), by = ver]
 dat[, rank := 1:.N, by = ver]
 
-ggplot(dat, aes(
-
+ggplot(dat, aes(rank, clines, colour = ver)) +
+  geom_point() +
+  geom_line(alpha = 0.3) +
+  theme_bw() +
+  ylim(0, NA) +
+  theme(legend.position = 'top') +
+  labs(colour = 'ABM version', x = 'File size rank', y = 'Cumulative lines of code')
+ggsave('lines.png', height = 8/2, width = 16/2)

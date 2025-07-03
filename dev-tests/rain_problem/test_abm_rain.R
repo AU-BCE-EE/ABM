c3616c127863553231f7a62ef805b1b408fd7b56
Frederik Rask Dalby <au277187@uni.au.dk>
2025-07-03 11:12:55 +0200

    rain_problem

---
+++ dev-tests/rain_problem/test_abm_rain.R
@@ -0,0 +1,21 @@
+rm(list = ls())
+library(data.table)
+
+devtools::load_all('../../../ABM')
+
+slurry_mass_dat <- fread('slurry_digestate_dat.csv')
+
+
+plot(slurry_mass_dat$time, slurry_mass_dat$slurry_mass)
+
+area = 1017.876
+rain = 1.9
+evap = 0
+
+slurry_mass_dat <- as.data.frame(slurry_mass_dat[, .(time, slurry_mass)])
+test <- abm(days = max(slurry_mass_dat$time), add_pars = list(evap = evap, slurry_mass = slurry_mass_dat, rain = rain, area = area))
+
+# add some slurry mass to avoid negative slurry mass in start
+
+slurry_mass_dat$slurry_mass <- slurry_mass_dat$slurry_mass + area * rain
+test <- abm(days = max(slurry_mass_dat$time), add_pars = list(evap = evap, slurry_mass = slurry_mass_dat, rain = rain, area = area))
